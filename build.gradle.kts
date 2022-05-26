ec4ac1512a52c305aed2c2fc109528388322d18d
P03W <P03W@users.noreply.github.com>
2022-05-25 20:40:25 -0400

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "io.github.p03w"
-version = "1.0.11"
+version = "1.1.0"
 description = "A gradle plugin to optimize built jars through individual file optimizations and increased compression"
 
 //region Dependencies
