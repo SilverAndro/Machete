a643f7b4763c4dcf023f3e3e33514cf49e9ffe27
P03W <P03W@users.noreply.github.com>
2022-05-31 12:09:04 -0400

    Always use absolute path

--- src/main/kotlin/io/github/p03w/machete/core/OxipngManager.kt
+++ src/main/kotlin/io/github/p03w/machete/core/OxipngManager.kt
@@ -10,6 +10,7 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 import java.nio.file.attribute.PosixFilePermissions
+import kotlin.io.path.absolute
 
 /**
  * Manages the Oxipng file
@@ -34,14 +35,14 @@ object OxipngManager {
 
     fun unpackOxipng(name: String) {
         // Get the file specific to this platform
-        val oxipng = when (platform) {
+        val oxipngStream = when (platform) {
             Platform.WINDOWS -> this::class.java.getResourceAsStream("/oxipng/oxipng-windows.exe")
             Platform.APPLE -> this::class.java.getResourceAsStream("/oxipng/oxipng-apple")
             Platform.LINUX -> this::class.java.getResourceAsStream("/oxipng/oxipng-linux")
         } ?: throw IllegalStateException("Could not unpack oxipng binary for platform ${platform.name}")
 
         // Grab the license
-        val license = this::class.java.getResourceAsStream("/oxipng/OXIPNG_LICENSE")
+        val licenseStream = this::class.java.getResourceAsStream("/oxipng/OXIPNG_LICENSE")
 
         // Make sure its .exe if windows, executable if linux/mac
         val file = if (platform == Platform.WINDOWS) {
@@ -52,7 +53,7 @@ object OxipngManager {
 
         logger.info("Detected platform is $platform for project $name")
         // Copy out oxipng, overwriting any previous copies
-        Files.copy(oxipng, file.toPath(), StandardCopyOption.REPLACE_EXISTING)
+        Files.copy(oxipngStream, file.toPath().absolute(), StandardCopyOption.REPLACE_EXISTING)
         // Try to make the file executable
         file.setExecutable(true)
         if (platform != Platform.WINDOWS) {
@@ -72,15 +73,22 @@ object OxipngManager {
         // Unpack or display the license, in compliance with the MIT license which states:
         // "The above copyright notice and this permission notice shall be included in all
         // copies or substantial portions of the Software."
-        if (license != null) {
+        if (licenseStream != null) {
             Files.copy(
-                license,
+                licenseStream,
                 file.resolveAndMakeSibling("OXIPNG_LICENSE").toPath(),
                 StandardCopyOption.REPLACE_EXISTING
             )
         } else {
             logger.warn("Failed to unpack oxipng license, oxipng is licensed under the MIT license, Copyright (c) 2016 Joshua Holmer")
         }
+
+        logger.info("Oxipng should be at ${file.absolutePath}")
+        if (file.exists().not()) {
+            logger.error("Oxipng didn't get created? Should be at ${file.absolutePath} but OS reports file doesn't exist, disabled png optimization.")
+            return
+        }
+
         this.oxipng = file.absolutePath
     }
 
